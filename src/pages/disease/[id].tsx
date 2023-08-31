6406c5a834d9206e7af40e2e49f5084bd7b22121
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-08-31 13:07:15 +0200

    add detail page style

--- src/pages/disease/[id].tsx
+++ src/pages/disease/[id].tsx
@@ -2,10 +2,33 @@ import { useRouter } from "next/router";
 import styled from "styled-components";
 import { diseases } from "../../lib/data";
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+`;
+
 const DiseaseDetailWrapper = styled.div`
+  max-width: 800px;
   padding: 20px;
+  background-color: #ffffff;
   border: 1px solid #ccc;
   border-radius: 5px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Heading = styled.h2`
+  margin-bottom: 10px;
+`;
+
+const SubHeading = styled.h3`
+  margin-top: 20px;
+`;
+
+const Paragraph = styled.p`
+  margin-top: 5px;
 `;
 
 export default function DiseaseDetail() {
@@ -18,18 +41,20 @@ export default function DiseaseDetail() {
   }
 
   return (
-    <DiseaseDetailWrapper>
-      <h2>{disease.name}</h2>
-      <h3>Beschreibung:</h3>
-      <p>{disease.description}</p>
-      <h3>Symptome:</h3>
-      <p>{disease.symptoms.join(", ")}</p>
-      <h3>Diagnose:</h3>
-      <p>{disease.diagnosis}</p>
-      <h3>Behandlung:</h3>
-      <p>{disease.treatment}</p>
-      <h3>Prävention:</h3>
-      <p>{disease.prevention}</p>
-    </DiseaseDetailWrapper>
+    <Container>
+      <DiseaseDetailWrapper>
+        <Heading>{disease.name}</Heading>
+        <SubHeading>Beschreibung:</SubHeading>
+        <Paragraph>{disease.description}</Paragraph>
+        <SubHeading>Symptome:</SubHeading>
+        <Paragraph>{disease.symptoms.join(", ")}</Paragraph>
+        <SubHeading>Diagnose:</SubHeading>
+        <Paragraph>{disease.diagnosis}</Paragraph>
+        <SubHeading>Behandlung:</SubHeading>
+        <Paragraph>{disease.treatment}</Paragraph>
+        <SubHeading>Prävention:</SubHeading>
+        <Paragraph>{disease.prevention}</Paragraph>
+      </DiseaseDetailWrapper>
+    </Container>
   );
 }
